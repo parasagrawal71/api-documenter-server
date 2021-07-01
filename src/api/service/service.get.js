0f529aaf725265a4fafadcc528fe73aa8f5f1be0
Paras Agrawal <pagrawal@nference.net>
2021-07-02 01:06:06 +0530

    endpoints count in service list

--- src/api/service/service.get.js
+++ src/api/service/service.get.js
@@ -1,4 +1,5 @@
 const ServiceModel = require("../../models/service.model");
+const EndpointModel = require("../../models/endpoint.model");
 const { successResponse, errorResponse } = require("../../utils/response.format");
 
 /**
@@ -7,8 +8,21 @@ const { successResponse, errorResponse } = require("../../utils/response.format"
 module.exports.getServices = (req, res, next) => {
   ServiceModel.find()
     .sort({ createdAt: -1 })
-    .then((services) => {
-      successResponse({ res, message: "List of services", data: services });
+    .then(async (services) => {
+      const updatedServices = await Promise.all(
+        services &&
+          services.map((service) => {
+            return EndpointModel.countDocuments({ serviceMID: service._id }).then((count) => {
+              console.log("count ", count);
+              return {
+                ...service._doc,
+                endpointsCount: count || 0,
+              };
+            });
+          })
+      );
+      console.log("updatedServices ", updatedServices);
+      successResponse({ res, message: "List of services", data: updatedServices });
     })
     .catch(next);
 };
