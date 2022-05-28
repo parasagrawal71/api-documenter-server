bcb2958eace9aeb085ec53c7d86f569dab1e24fc
Paras Agrawal <parasagrawal71@gmail.com>
2022-05-28 17:01:46 +0530

    Update user.post.js

--- src/api/user/user.post.js
+++ src/api/user/user.post.js
@@ -5,6 +5,8 @@ const { successResponse, errorResponse } = require("../../utils/response.format"
  * @description Function to create a user
  */
 module.exports.createUser = (req, res, next) => {
+  const reqBody = req.body || {};
+  reqBody.editAccess = ["JSON Placeholder"];
   const userObj = UserModel(req.body);
   userObj
     .save()
